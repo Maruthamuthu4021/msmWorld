e18836fd835ecfc31742a3702be41f08275923eb
MSM <MSM@DESKTOP-828BAF7>
2021-05-09 19:13:55 +0530

    first commit

---
+++ HelloWorld-git/src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] arg)
+	{
+		System.out.println("HI WELCOME TO");
+		System.out.println("|=>MSM WORLD<=|");
+	}
+}
